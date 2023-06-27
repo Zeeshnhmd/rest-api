4e360e067abf896bf8cc6e3941f22e5a1d89ce73
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-06-28 02:53:05 +0530

    feat: added errorhandler in server.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ import express from 'express';
 
 import { APP_PORT } from './config';
 import routes from './routes';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
+app.use(express.json());
 app.use('/api', routes);
+
+app.use(errorHandler);
 
 app.listen(APP_PORT, () => console.log(`Listening on port ${APP_PORT}.`));
