dbbccf6e405f6894e5fb3bac273b19c0dfd89e2e
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-06-29 01:23:58 +0530

    feat: database inilization

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-import { APP_PORT } from './config';
+import { APP_PORT, DB_URL } from './config';
 import routes from './routes';
 import errorHandler from './middlewares/errorHandler';
 
 const app = express();
+
+mongoose.connect(DB_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+	console.log('DB connected...');
+});
+
 app.use(express.json());
 app.use('/api', routes);
 
