8cb886d494b6e87c4d1fe2644ae43c7d12f2fdd2
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-07-03 01:08:39 +0530

    feat: login route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import { registerController } from '../controllers';
+import express from "express";
+import { registerController, loginController } from "../controllers";
 
 const routes = express.Router();
 
-routes.post('/register', registerController.register);
+routes.post("/register", registerController.register);
+routes.post("/login", loginController.login);
 
 export default routes;
