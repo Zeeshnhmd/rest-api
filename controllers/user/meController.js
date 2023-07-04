d9b889068b8bb6570ebcea9dc429214a2c70bc55
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-07-04 23:18:08 +0530

    feat: me controllers

---
+++ controllers/user/meController.js
@@ -0,0 +1,5 @@
+const meController = {
+	me(req, res, next) {},
+};
+
+export default meController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { registerController, loginController } from "../controllers";
+import {
+	registerController,
+	loginController,
+	meController,
+} from "../controllers";
 
 const routes = express.Router();
 
 routes.post("/register", registerController.register);
 routes.post("/login", loginController.login);
+routes.get("/me", meController.me);
 
 export default routes;
