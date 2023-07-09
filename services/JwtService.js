3146ba4743b842109b29d5c7bf65f2ec580afced
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-07-09 22:34:16 +0530

    feat: jwt verify

--- services/JwtService.js
+++ services/JwtService.js
@@ -6,6 +6,10 @@ class JwtService {
 	static sign(payload, expiry = '60s', secret = JWT_SECRET) {
 		return jwt.sign(payload, secret, { expiresIn: expiry });
 	}
+
+	static verify(payload, secret = JWT_SECRET) {
+		return jwt.verify(payload, secret);
+	}
 }
 
 export default JwtService;
