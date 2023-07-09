3718fe1ca5e112cabf9c7dc21303cf88a4412863
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-07-09 22:33:47 +0530

    feat: wrongcredentials, unauthorized, notfound customerrorhandler

--- services/CustomErrorHandler.js
+++ services/CustomErrorHandler.js
@@ -9,8 +9,16 @@ class CustomErrorHandler extends Error {
 		return new CustomErrorHandler(409, message);
 	}
 
-	static wrongCredentials(message = "Username or password in wrong") {
+	static wrongCredentials(message = 'Username or password in wrong') {
 		return new CustomErrorHandler(401, message);
+	}
+
+	static unAuthorized(message = 'UnAuthorized') {
+		return new CustomErrorHandler(401, message);
+	}
+
+	static notFound(message = '404 not found') {
+		return new CustomErrorHandler(404, message);
 	}
 }
 
